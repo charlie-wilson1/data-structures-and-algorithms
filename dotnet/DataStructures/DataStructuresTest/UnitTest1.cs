2c24a5a99d4ca3e03bcb50d32e054ad5031dd297
johnxferraro <johnxferraro@gmail.com>
2021-07-19 23:07:48 -0700

    Added InsertBefore, InsertAfter, Append methods for linkedlist

--- dotnet/DataStructures/DataStructuresTest/UnitTest1.cs
+++ dotnet/DataStructures/DataStructuresTest/UnitTest1.cs
@@ -1,15 +1,60 @@
 using System;
+using DataStructures.Classes;
 using Xunit;
-using DataStructures;
 
 namespace DataStructuresTest
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
 
-        }
+  public class UnitTest1
+  {
+    public Node node = new Node(10);
+    [Fact]
+    public void ProofOfLife()
+    {
+      LinkedList list = new LinkedList();
+      Assert.NotNull(list);
     }
+    [Fact]
+    public void ProofOfHeadAndNext()
+    {
+      LinkedList list = new LinkedList();
+      list.Insert(3);
+      list.Insert(6);
+      list.Insert(9);
+      Assert.Equal(9, list.Head.Value);
+      Assert.Equal(6, list.Head.Next.Value);
+    }
+    [Fact]
+    public void ProofOfInsertAndList()
+    {
+      LinkedList list = new LinkedList();
+      list.Insert(3);
+      list.Insert(6);
+      list.Insert(9);
+      Assert.True(list.Includes(6));
+      Assert.False(list.Includes(12));
+    }
+    [Fact]
+    public void ProofOflistAndPrint()
+    {
+      LinkedList list = new LinkedList();
+      list.Insert(3);
+      list.Insert(6);
+      list.Insert(9);
+      Assert.Equal("(9) -> (6) -> (3) -> NULL", list.Print());
+    }
+    [Fact]
+    public void ProofOfInsertBefore()
+    {
+      LinkedList list = new LinkedList();
+      list.Insert(3);
+      list.Insert(6);
+      list.Insert(9);
+      // Test's inserting at middle of list
+      list.InsertBefore(6, 12);
+      // Test's inserting at Head of list
+      list.InsertBefore(9, 15);
+      Assert.Equal("(15) -> (9) -> (12) -> (6) -> (3) -> NULL", list.Print());
+    }
+  }
 }
