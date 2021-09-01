f692bd6548c0e5f36034c35288383c914fffdea3
johnxferraro <johnxferraro@gmail.com>
2021-08-31 21:05:20 -0700

    Update UnitTest1.cs

--- dotnet/DataStructures/DataStructuresTests/GraphTests/UnitTest1.cs
+++ dotnet/DataStructures/DataStructuresTests/GraphTests/UnitTest1.cs
@@ -56,5 +56,35 @@ namespace GraphTests
 
       Assert.Null(graph.GetNodes());
     }
+   
+    [Fact]
+     static void CanAddNodesAndEdges()
+    {
+      Graph<int> testGraph = new();
+      Vertex<int> node1 = testGraph.AddNode(7);
+      Vertex<int> node2 = testGraph.AddNode(9);
+      Vertex<int> node3 = testGraph.AddNode(3);
+      testGraph.AddEdge(node1, node2, 12);
+      testGraph.AddEdge(node2, node3, 36);
+      testGraph.AddEdge(node3, node1, 24);
+      List<Vertex<int>> nodeNeighbors = testGraph.GetNeighbors(node1);
+      Assert.Equal(node2, nodeNeighbors[0]);
+      List<Vertex<int>> allNodes = testGraph.GetNodes();
+      Assert.Equal(node1, allNodes[0]);
+      Assert.Equal(node2, allNodes[1]);
+      Assert.Equal(node3, allNodes[2]);
+    }
+
+    [Fact]
+    public void CanTrackSize()
+    {
+      Graph<int> testGraph = new();
+      Assert.Equal(0, testGraph.GetSize());
+      testGraph.AddNode(7);
+      testGraph.AddNode(9);
+      testGraph.AddNode(3);
+      Assert.Equal(3, testGraph.GetSize());
+
+     
   }
 }
