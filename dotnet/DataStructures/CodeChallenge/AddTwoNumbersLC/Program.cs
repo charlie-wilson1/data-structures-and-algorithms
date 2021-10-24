90ce125a099f0aecc57179affc8e6aacbfef458a
johnxferraro <johnxferraro@gmail.com>
2021-10-23 22:10:54 -0700

    Partially complete AddTwoNumbers LC

---
+++ dotnet/DataStructures/CodeChallenge/AddTwoNumbersLC/Program.cs
@@ -0,0 +1,19 @@
+using CodeChallenge16_Tree_Max.Classes;
+using System;
+
+namespace AddTwoNumbersLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+      static int AddTwoNumbers(Node<int> node, Node<int> node2)
+    {
+
+      return 1;
+    }
+    }
+}
