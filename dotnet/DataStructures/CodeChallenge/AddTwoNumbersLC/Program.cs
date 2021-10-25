868506cd8c726b8b182e27cf7ff6a417355823c0
johnxferraro <johnxferraro@gmail.com>
2021-10-24 21:37:48 -0700

    Add Two Nums progress

--- dotnet/DataStructures/CodeChallenge/AddTwoNumbersLC/Program.cs
+++ dotnet/DataStructures/CodeChallenge/AddTwoNumbersLC/Program.cs
@@ -1,8 +1,23 @@
-using CodeChallenge16_Tree_Max.Classes;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeChallenge18.Classes;
 
 namespace AddTwoNumbersLC
 {
+  class Node<T>
+  {
+    public T Value { get; set; }
+
+    public Node<T> Next { get; set; }
+
+    public Node(T val)
+    {
+      Value = val;
+    }
+  }
     class Program
     {
         static void Main(string[] args)
@@ -10,10 +25,19 @@ namespace AddTwoNumbersLC
 
         }
 
+    // NAIVE SOLUTION
       static int AddTwoNumbers(Node<int> node, Node<int> node2)
-    {
+      {
+        List<int> l1 = new List<int>();
+        List<int> l2 = new List<int>();
+
+        while (node != null)
+        {
+          l1.Add(node.Value);
+          node = node.Next;
+        }
 
       return 1;
     }
-    }
+  }
 }
