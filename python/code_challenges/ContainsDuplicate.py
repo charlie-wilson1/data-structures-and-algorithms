0f6b4c488c50c6d8497ea1862388ae16ba94ac67
johnxferraro <johnxferraro@gmail.com>
2021-11-23 17:27:18 -0800

    Added additional comments for clarity

--- python/code_challenges/ContainsDuplicate.py
+++ python/code_challenges/ContainsDuplicate.py
@@ -4,6 +4,7 @@ class Solution:
     def ContainsDuplicate(self, nums: List[int]) -> bool:
         checkerArr = {}
         for i, num in enumerate(nums):
+            # If the number that we are currently on exists within the checker array return true.
             if num in checkerArr:
                 return True
             checkerArr[num] = i
