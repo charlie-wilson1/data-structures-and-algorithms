64877fd16b297fa1bd3661e3a5f1242ccac48936
johnxferraro <johnxferraro@gmail.com>
2022-01-15 17:01:39 -0800

    Added comments to Code Challenge 33/31, initialization of 3sum file

--- javascript/code-challenges/SameTree.js
+++ javascript/code-challenges/SameTree.js
@@ -9,6 +9,27 @@
 // If end of recursive call is reached without returning false, return true. 
 // this problem is considered pre-order traversal as the root node is visited first, then left, then right.
 
-const SameTree = (p, q) => {
-  if (p !== q ) return false;
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val);
+  this.left = (left === undefined ? 0: left);
+  this.right = (right === undefined ? 0: right);
+}
+
+const isSameTree = function(p, q) {
+  // isSame will be used and returned at the very end of the call stack.
+  var isSame = true;
+  recursiveCall(p, q, isSame);
+  return isSame;
+};
+
+const recursiveCall = (p, q, isSame) => {
+  if (p === null && q === null) return 0;
+  if (p !== q || p === null && q !== null || q === null && p !== null){
+    isSame = false;
+    return 0;
+  }
+
+  recursiveCall(p.left, q.left);
+  recursiveCall(p.right, q.right);
+  return 0;
 };
